a0517e216f9673281848ef49087b09199e9f2655
Your Namemary3l <marmabs692@gmail.com>
2023-05-21 13:22:17 +0800

    exer 7 done

--- furnitureshopwebsitetemplate/index.php
+++ furnitureshopwebsitetemplate/index.php
@@ -1,6 +1,122 @@
 <!DOCTYPE html>
 <!-- Website template by freewebsitetemplates.com -->
 <html>
+<style>
+	.modal-container {
+		display: none;
+		position: fixed;
+		z-index: 9990;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background-color: rgba(0, 0, 0, 0.5);
+		/* Semi-transparent black background */
+	}
+
+	.modal-container.open {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.modal-content {
+		background-color: #D35400;
+		color: #fff;
+		max-width: 1000px;
+		margin: auto;
+		padding: 80px;
+		box-sizing: border-box;
+		text-align: center;
+		position: relative;
+		top: 10%;
+		border-radius: 8px;
+		box-shadow: 0 2px 4px rgba(0, 0, 0, 0.3);
+	}
+
+	.modal-content input {
+		height: 40px;
+		border-radius: 4px;
+		border: none;
+		padding: 8px;
+		margin-bottom: 10px;
+	}
+
+	.modal-content button {
+		padding: 8px 16px;
+		background-color: #EF6C00;
+		border: none;
+		color: white;
+		border-radius: 4px;
+		cursor: pointer;
+		transition: background-color 0.3s ease;
+	}
+
+	.modal-content button:hover {
+		background-color: #E65100;
+	}
+
+	.modal-content .close {
+		margin-right: 10px;
+		margin-top: 5px;
+		color: #fff;
+		opacity: 0.8;
+		position: absolute;
+		top: 20px;
+		right: 20px;
+		font-size: 24px;
+		cursor: pointer;
+	}
+
+	/* Add the rest of your CSS styles for chatContainer, message, inputContainer, messageInput, and sendButton here */
+	#chatContainer {
+		height: 400px;
+		overflow-y: scroll;
+		padding: 10px;
+		background-color: #F57C00;
+		border-radius: 8px;
+		margin-bottom: 20px;
+	}
+
+	.message {
+		background-color: #D35400;
+		padding: 10px;
+		margin-bottom: 10px;
+		border-radius: 8px;
+		color: #fff;
+	}
+
+	#inputContainer {
+		padding: 10px;
+		background-color: #FF9800;
+		border-radius: 8px;
+		margin-bottom: 20px;
+		display: flex;
+	}
+
+	#messageInput {
+		flex: 1;
+		padding: 8px;
+		border: none;
+		border-radius: 4px;
+		margin-right: 5px;
+	}
+
+	#sendButton {
+		padding: 8px 16px;
+		background-color: #EF6C00;
+		border: none;
+		color: white;
+		border-radius: 4px;
+		cursor: pointer;
+		transition: background-color 0.3s ease;
+	}
+
+	#sendButton:hover {
+		background-color: #E65100;
+	}
+</style>
+
 
 <head>
 	<meta charset="UTF-8">
@@ -23,10 +139,13 @@
 					if ($_COOKIE['type'] == 'admin') {
 						echo '<li><a href="calendar.php">Calendar</a></li>';
 						echo '<li><a href="products.php">Products</a></li>';
+						echo '<li><a href="#" onclick="openFormPopup()">Open Chat</a></li>';
 					} elseif ($_COOKIE['type'] == 'customer') {
 						echo '<li><a href="menu.php">Menu</a></li>';
 						echo '<li><a href="cart.php">Cart</a></li>';
 						echo '<li><a href="myorders.php">My Orders</a></li>';
+						echo '<li><a href="#" onclick="openFormPopup()">Open Chat</a></li>';
+
 					}
 				}
 				?>
@@ -183,6 +302,16 @@
 			</ul>
 		</div>
 	</div>
+	<div id="popup-container" class="modal-container" style="display: none;">
+		<div id="popup-window" class="modal-content">
+			<button type="button" class="close" onclick="closeFormPopup()">&times;</button>
+			<div id="chatContainer"></div>
+			<div id="inputContainer">
+				<input type="text" id="messageInput" placeholder="Type a message...">
+				<button id="sendButton">Send</button>
+			</div>
+		</div>
+	</div>
 	<div id="footer">
 		<div>
 			<div>
@@ -213,6 +342,111 @@
 			Copyright &copy; Website name all rights reserved.
 		</p>
 	</div>
+
+	<script>
+		document.addEventListener("DOMContentLoaded", function () {
+			var chatContainer = document.getElementById("chatContainer");
+			var messageInput = document.getElementById("messageInput");
+			var sendButton = document.getElementById("sendButton");
+
+			// Function to generate chat message HTML
+			function generateMessageHTML(sender, message, timestamp, userType) {
+				var userLabel = sender === "Admin" ? "" : "Customer";
+				var messageElement = document.createElement("div");
+				messageElement.classList.add("message");
+				messageElement.innerHTML = `
+			<div><strong>${userLabel} ${sender}</strong></div>
+			<div>${message}</div>
+			<div>${timestamp}</div>
+			<button class="reply-button" data-recipient="${sender}">Reply</button>
+		`;
+				return messageElement;
+			}
+
+			// Function to handle reply button click
+			function handleReplyClick(event) {
+				var recipient = event.target.dataset.recipient;
+				var replyInput = document.getElementById("messageInput");
+				replyInput.value = "";
+				replyInput.placeholder = "Reply to " + recipient;
+				replyInput.dataset.recipient = recipient; // Set the recipient value on the dataset
+			}
+
+
+			// Attach event listener to reply buttons
+			chatContainer.addEventListener("click", function (event) {
+				if (event.target.classList.contains("reply-button")) {
+					handleReplyClick(event);
+				}
+			});
+
+			// Event listener for sending a message
+			sendButton.addEventListener("click", function () {
+				var newMessage = messageInput.value;
+				var recipient = messageInput.dataset.recipient; // Get the recipient from the messageInput dataset
+
+				if (newMessage.trim() !== "" && recipient) {
+					// Send the message to the server
+					var formData = new FormData();
+					formData.append("message", newMessage);
+					formData.append("recipient", recipient);
+
+					fetch("insert_chat_data.php", {
+						method: "POST",
+						body: formData,
+					})
+						.then(function (response) {
+							return response.json();
+						})
+						.then(function (data) {
+							// Successfully inserted the message, update chat messages
+							getChatMessages();
+						})
+						.catch(function (error) {
+							console.log("Error sending chat message:", error);
+						});
+
+					messageInput.value = "";
+					messageInput.dataset.recipient = ""; // Clear the recipient after sending the message
+				}
+			});
+
+			// Function to render chat messages
+			function renderChatMessages(messages) {
+				chatContainer.innerHTML = "";
+				for (var i = 0; i < messages.length; i++) {
+					var { sender, message, timestamp } = messages[i];
+					var messageHTML = generateMessageHTML(sender, message, timestamp);
+					chatContainer.appendChild(messageHTML);
+				}
+			}
+
+			// Function to retrieve chat messages from the server
+			function getChatMessages() {
+				fetch("read_chat_data.php")
+					.then(function (response) {
+						return response.json();
+					})
+					.then(function (data) {
+						renderChatMessages(data);
+					})
+					.catch(function (error) {
+						console.log("Error fetching chat messages:", error);
+					});
+			}
+
+			// Initial rendering of chat messages
+			getChatMessages();
+		});
+
+		function openFormPopup() {
+			document.getElementById('popup-container').style.display = 'flex';
+		}
+
+		function closeFormPopup() {
+			document.getElementById('popup-container').style.display = 'none';
+		}
+	</script>
 </body>
 
 </html>
